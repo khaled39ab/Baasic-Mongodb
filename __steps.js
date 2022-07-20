157f5d74bb53c0d1d60a3046334c9f856944d441
khaled39ab <khaled39ab@gmail.com>
2022-07-20 22:58:11 +0600

    listing the steps

---
+++ __steps.js
@@ -0,0 +1,28 @@
+/** 
+ * ------------------------
+ * Basic node server
+ * ------------------------
+ * 1- create a node server with 5 steps.
+ * 1.1- Create a folder
+ * 1.2- npm init
+ * 1.3- npm i express cors mongodb
+ * 1.4- edit script in package.json
+ * 1.5- create index.js file
+ * 1.6- use 5 steps to create a node server
+ * 
+ * ---------------------------
+ * create Atlas account
+ * ---------------------------
+ * 1- sign up with google
+ * 2- create user with username and password
+ * 3- Go to network access -> id address : allow all
+ * 4- then go to Database -> Connect -> copy the code and paste in index.js
+ * 
+ * ---------------------------
+ * CRUD operation
+ * ---------------------------
+ * 1- Go to page node mongodb crud -> fundamental -> usage example
+ * 2- create async run function
+ * 
+ * 
+ * **/
