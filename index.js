da646eb445a12ced01d95ec535528297b845a91f
khaled39ab <khaled39ab@gmail.com>
2022-07-20 12:40:48 +0600

    signup in mongodb and connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,10 +9,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-app.get('/', (req, res) =>{
+
+//username:dbuser1 
+//password:S6HEPXaS.!nA4c4
+
+
+const uri = "mongodb+srv://dbuser1:S6HEPXaS.!nA4c4@cluster0.exvps.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('db connected');
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
     res.send('Running my node in CRUD server')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('CRUD server is running');
 })
