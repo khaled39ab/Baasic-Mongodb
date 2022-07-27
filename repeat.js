ce937135e1b15c2ccc1d4b02805e9e8c0da18967
khaled39ab <khaled39ab@gmail.com>
2022-07-27 21:52:42 +0600

    repeat post data received

--- repeat.js
+++ repeat.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -14,10 +17,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const userCollection = client.db("repeatExpress").collection("user");
-        const user = {name: 'Mamun Munna', email: 'mamu@gmail.com'};
 
-        const result = await userCollection.insertOne(user);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            console.log('new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result)
+        });
+
+        
     }
     finally {
         // await client.close();
